aa71de137d4cb1a3db3d0a85261431b3387c052c
DaljotKaur <daljotkaur@edu.sait.ca>
2024-03-04 16:45:01 -0700

    week

--- app/page.js
+++ app/page.js
@@ -107,23 +107,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-        <a
-    href="/week-3"  // Adjust the URL accordingly
-    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-  >
-    <h2 className={`mb-3 text-2xl font-semibold`}>
-      Week 3
-      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-        -&gt;
-      </span>
-    </h2>
-    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-      Your description for Week 3 here.
-    </p>
-  </a>
-
-
-      </div>
+       </div>
     </main>
   );
 }
